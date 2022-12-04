1f4402418f2424c581ef3eff38d9e4e842f35ad1
Christian Hesels <christian.hesels@fokus.fraunhofer.de>
2022-12-04 16:53:59 +0100

    Allow * operator

--- file_observer.py
+++ file_observer.py
@@ -1,7 +1,8 @@
 import os
 import time
-
+import glob
 import shutil
+from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
@@ -26,7 +27,7 @@ class FileObserverHandler(FileSystemEventHandler):
 
 class FileObserver:
     def __init__(self, source_path, target_path, files) -> None:
-        print("Create File Observer")
+        print("Start File Observer")
         self.source_path = source_path.rstrip("/")
         self.target_path = target_path.rstrip("/")
         self.files = files
@@ -34,10 +35,23 @@ class FileObserver:
         self.start_observer()
 
     def init_folder_structure(self):
-        for file in self.files:
-            file_path_source = self.source_path + "/" + file.lstrip("/")
-            file_path_target = self.target_path + "/" + file.lstrip("/")
-            if os.path.isfile(file_path_source) and os.path.exists(file_path_source):
+        for file_path in self.files:
+            file_path_source = self.source_path + "/" + file_path.lstrip("/")
+            file_path_target = self.target_path + "/" + file_path.lstrip("/")
+            if "*" in file_path and "." in file_path and "/" not in file_path:
+                ext_split = file_path.split(".")
+                if len(ext_split) != 2:
+                    continue
+                for file_with_ext in Path(self.source_path).rglob(file_path):
+                    if os.path.isfile(file_with_ext):
+                        new_target_path = str(file_with_ext).replace(
+                            self.source_path, self.target_path
+                        )
+
+                        os.makedirs(os.path.dirname(new_target_path), exist_ok=True)
+                        shutil.copyfile(file_with_ext, new_target_path)
+                    self.files.append(str(file_with_ext).replace(self.source_path, ""))
+            elif os.path.isfile(file_path_source) and os.path.exists(file_path_source):
                 if os.path.exists(file_path_target):
                     if os.path.samefile(file_path_source, file_path_target):
                         continue
